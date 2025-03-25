dc431778f1eaf455a74a7dc95e6cdc7f1c369926
Venkatesh Babu <dbvb2k@gmail.com>
2025-03-25 15:46:30 +0530

    Initial commit

---
+++ map.py
@@ -0,0 +1,467 @@
+# Self Driving Car
+
+# Importing the libraries
+import numpy as np
+from random import random, randint
+import matplotlib.pyplot as plt
+import time
+import torch
+import os
+
+# Importing the Kivy packages
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.graphics import Color, Ellipse, Line
+from kivy.config import Config
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from kivy.core.image import Image as CoreImage
+from PIL import Image as PILImage
+from kivy.graphics.texture import Texture
+
+# Importing the TD3 implementation
+from td3 import TD3, ReplayBuffer, device
+
+# Create images directory if it doesn't exist
+if not os.path.exists("./images"):
+    os.makedirs("./images")
+
+# Create MASK1.png if it doesn't exist
+if not os.path.exists("./images/MASK1.png"):
+    # Create a white circle on black background
+    mask1 = np.zeros((100, 100), dtype=np.uint8)
+    center = (50, 50)
+    radius = 20
+    y, x = np.ogrid[-center[0]:100-center[0], -center[1]:100-center[1]]
+    mask1[x*x + y*y <= radius*radius] = 255
+    PILImage.fromarray(mask1).save("./images/MASK1.png")
+
+# Create mask.png if it doesn't exist
+if not os.path.exists("./images/mask.png"):
+    # Create a black image (will be filled with sand later)
+    mask = np.zeros((1429, 660), dtype=np.uint8)
+    PILImage.fromarray(mask).save("./images/mask.png")
+
+# Adding this line if we don't want the right click to put a red point
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '1429')
+Config.set('graphics', 'height', '660')
+
+# Introducing last_x and last_y, used to keep the last point in memory when we draw the sand on the map
+last_x = 0
+last_y = 0
+n_points = 0
+length = 0
+
+# Getting our AI, which we call "brain", and that contains our neural network that represents our Q-function
+# Initialize TD3 with state_dim=5 (3 signals + 2 orientations) and action_dim=1 (rotation)
+brain = TD3(state_dim=5, action_dim=1, max_action=5.0)
+action2rotation = [0,5,-5]
+last_reward = 0
+scores = []
+im = CoreImage("./images/MASK1.png")
+
+# Add episode tracking variables
+total_timesteps = 0
+episode_num = 1
+episode_reward = 0
+evaluation_rewards = []
+EVALUATION_INTERVAL = 5000  # Evaluate every 5000 timesteps
+
+# Define the three target points (A1, A2, A3) globally
+target_points = [
+    {'x': 1220, 'y': 618},  # A1
+    {'x': 200, 'y': 285},   # A2
+    {'x': 700, 'y': 265}    # A3
+]
+
+# Define the starting position for the car
+start_position = {
+    'x': 580,  # Starting x coordinate
+    'y': 450   # Starting y coordinate
+}
+
+# Initialize replay buffer for TD3
+replay_buffer = ReplayBuffer(max_size=1e6)
+
+# textureMask = CoreImage(source="./kivytest/simplemask1.png")
+
+# Initializing the map
+first_update = True
+def init():
+    global sand
+    global goal_x
+    global goal_y
+    global first_update
+    global current_target_index
+    
+    current_target_index = 0
+    
+    sand = np.zeros((longueur,largeur))
+    img = PILImage.open("./images/mask.png").convert('L')
+    sand = np.asarray(img)/255
+    goal_x = target_points[0]['x']
+    goal_y = target_points[0]['y']
+    first_update = False
+    global swap
+    swap = 0
+
+# Initializing the last distance
+last_distance = 0
+
+# Creating the car class
+
+class Car(Widget):
+    
+    angle = NumericProperty(0)
+    rotation = NumericProperty(0)
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+    sensor1_x = NumericProperty(0)
+    sensor1_y = NumericProperty(0)
+    sensor1 = ReferenceListProperty(sensor1_x, sensor1_y)
+    sensor2_x = NumericProperty(0)
+    sensor2_y = NumericProperty(0)
+    sensor2 = ReferenceListProperty(sensor2_x, sensor2_y)
+    sensor3_x = NumericProperty(0)
+    sensor3_y = NumericProperty(0)
+    sensor3 = ReferenceListProperty(sensor3_x, sensor3_y)
+    signal1 = NumericProperty(0)
+    signal2 = NumericProperty(0)
+    signal3 = NumericProperty(0)
+
+    def move(self, rotation):
+        self.pos = Vector(*self.velocity) + self.pos
+        self.rotation = rotation
+        self.angle = self.angle + self.rotation
+        self.sensor1 = Vector(30, 0).rotate(self.angle) + self.pos
+        self.sensor2 = Vector(30, 0).rotate((self.angle+30)%360) + self.pos
+        self.sensor3 = Vector(30, 0).rotate((self.angle-30)%360) + self.pos
+        self.signal1 = int(np.sum(sand[int(self.sensor1_x)-10:int(self.sensor1_x)+10, int(self.sensor1_y)-10:int(self.sensor1_y)+10]))/400.
+        self.signal2 = int(np.sum(sand[int(self.sensor2_x)-10:int(self.sensor2_x)+10, int(self.sensor2_y)-10:int(self.sensor2_y)+10]))/400.
+        self.signal3 = int(np.sum(sand[int(self.sensor3_x)-10:int(self.sensor3_x)+10, int(self.sensor3_y)-10:int(self.sensor3_y)+10]))/400.
+        if self.sensor1_x>longueur-10 or self.sensor1_x<10 or self.sensor1_y>largeur-10 or self.sensor1_y<10:
+            self.signal1 = 10.
+        if self.sensor2_x>longueur-10 or self.sensor2_x<10 or self.sensor2_y>largeur-10 or self.sensor2_y<10:
+            self.signal2 = 10.
+        if self.sensor3_x>longueur-10 or self.sensor3_x<10 or self.sensor3_y>largeur-10 or self.sensor3_y<10:
+            self.signal3 = 10.
+        
+
+class Ball1(Widget):
+    pass
+class Ball2(Widget):
+    pass
+class Ball3(Widget):
+    pass
+
+# Creating the target marker class
+class TargetMarker(Widget):
+    def __init__(self, **kwargs):
+        super(TargetMarker, self).__init__(**kwargs)
+        with self.canvas:
+            Color(0.5, 0, 0)  # Dark red color for initial state
+            self.circle = Ellipse(size=(20, 20))  # Size of the marker
+            self.circle.pos = (self.x - 10, self.y - 10)  # Center the circle
+
+    def update_pos(self, x, y):
+        self.x = x
+        self.y = y
+        self.circle.pos = (x - 10, y - 10)
+
+# Creating the start marker class
+class StartMarker(Widget):
+    def __init__(self, **kwargs):
+        super(StartMarker, self).__init__(**kwargs)
+        with self.canvas:
+            Color(0, 0, 1)  # Blue color for start position
+            self.circle = Ellipse(size=(25, 25))  # Slightly larger than target markers
+            self.circle.pos = (self.x - 12.5, self.y - 12.5)  # Center the circle
+            # Add a border
+            Color(1, 1, 1)  # White border
+            self.border = Ellipse(size=(30, 30))
+            self.border.pos = (self.x - 15, self.y - 15)
+        
+        # Add the 'S' label
+        self.label = Label(
+            text='S',
+            color=(0, 0, 1, 1),  # Blue color
+            font_size='16sp',
+            bold=True,
+            center_x=self.center_x,
+            center_y=self.center_y
+        )
+        self.add_widget(self.label)
+
+    def update_pos(self, x, y):
+        self.x = x
+        self.y = y
+        self.circle.pos = (x - 12.5, y - 12.5)
+        self.border.pos = (x - 15, y - 15)
+        self.label.center_x = x
+        self.label.center_y = y
+
+# Creating the game class
+
+class Game(Widget):
+
+    car = ObjectProperty(None)
+    ball1 = ObjectProperty(None)
+    ball2 = ObjectProperty(None)
+    ball3 = ObjectProperty(None)
+    target_markers = []  # List to store target markers
+    start_marker = None  # Start position marker
+
+    def serve_car(self):
+        # Set the car's initial position to the defined start position
+        self.car.x = start_position['x']
+        self.car.y = start_position['y']
+        self.car.velocity = Vector(6, 0)
+        
+        # Initialize start marker
+        if self.start_marker is None:
+            self.start_marker = StartMarker()
+            self.start_marker.update_pos(start_position['x'], start_position['y'])
+            self.add_widget(self.start_marker)
+        
+        # Initialize target markers
+        self.target_markers = []
+        for point in target_points:
+            marker = TargetMarker()
+            marker.update_pos(point['x'], point['y'])
+            self.add_widget(marker)
+            self.target_markers.append(marker)
+
+    def update(self, dt):
+        global brain
+        global last_reward
+        global scores
+        global last_distance
+        global goal_x
+        global goal_y
+        global longueur
+        global largeur
+        global swap
+        global target_points
+        global current_target_index
+        global replay_buffer
+        global total_timesteps
+        global episode_num
+        global episode_reward
+        global evaluation_rewards
+
+        longueur = self.width
+        largeur = self.height
+        if first_update:
+            init()
+
+        # Get current state
+        xx = goal_x - self.car.x
+        yy = goal_y - self.car.y
+        orientation = Vector(*self.car.velocity).angle((xx,yy))/180.
+        state = [self.car.signal1, self.car.signal2, self.car.signal3, orientation, -orientation]
+        
+        # Select action using TD3
+        action = brain.select_action(np.array(state))
+        # Convert continuous action to discrete rotation
+        rotation_idx = int(np.clip((action[0] + 5) / 5, 0, 2))  # Map [-5,5] to [0,2]
+        rotation = action2rotation[rotation_idx]
+        
+        # Store the old position for reward calculation
+        old_x, old_y = self.car.x, self.car.y
+        
+        # Move the car
+        self.car.move(rotation)
+        
+        # Get new state
+        new_xx = goal_x - self.car.x
+        new_yy = goal_y - self.car.y
+        new_orientation = Vector(*self.car.velocity).angle((new_xx,new_yy))/180.
+        new_state = [self.car.signal1, self.car.signal2, self.car.signal3, new_orientation, -new_orientation]
+        
+        # Update distance
+        distance = np.sqrt((self.car.x - goal_x)**2 + (self.car.y - goal_y)**2)
+        
+        # Update ball positions
+        self.ball1.pos = self.car.sensor1
+        self.ball2.pos = self.car.sensor2
+        self.ball3.pos = self.car.sensor3
+
+        # Update target markers colors based on current target
+        for i, marker in enumerate(self.target_markers):
+            with marker.canvas:
+                marker.canvas.clear()
+                if i == current_target_index:
+                    Color(0, 1, 0)  # Green for current target
+                else:
+                    Color(0.5, 0, 0)  # Dark red for other targets
+                marker.circle = Ellipse(size=(20, 20))
+                marker.circle.pos = (marker.x - 10, marker.y - 10)
+
+        # Calculate reward
+        if sand[int(self.car.x),int(self.car.y)] > 0:
+            self.car.velocity = Vector(0.5, 0).rotate(self.car.angle)
+            last_reward = -1
+        else:
+            self.car.velocity = Vector(2, 0).rotate(self.car.angle)
+            # Base reward for being on track
+            last_reward = -0.1
+            
+            # Reward for getting closer to target
+            if distance < last_distance:
+                last_reward = 0.5
+            else:
+                # Small penalty for moving away from target
+                last_reward = -0.2
+            
+            # Penalty for sharp turns to discourage circular movements
+            if abs(rotation) > 3:
+                last_reward -= 0.3
+            
+            # Bonus for maintaining good speed
+            if abs(self.car.velocity[0]) > 1.5:
+                last_reward += 0.1
+
+        # Additional penalties for hitting boundaries
+        if self.car.x < 5:
+            self.car.x = 5
+            last_reward = -1
+        if self.car.x > self.width - 5:
+            self.car.x = self.width - 5
+            last_reward = -1
+        if self.car.y < 5:
+            self.car.y = 5
+            last_reward = -1
+        if self.car.y > self.height - 5:
+            self.car.y = self.height - 5
+            last_reward = -1
+
+        # Check if reached target
+        if distance < 25:
+            # Switch to next target point
+            current_target_index = (current_target_index + 1) % len(target_points)
+            goal_x = target_points[current_target_index]['x']
+            goal_y = target_points[current_target_index]['y']
+            last_reward = 5  # Increased bonus reward for reaching target
+            print(f"Switching to target point {current_target_index + 1}")
+
+        # Add transition to replay buffer
+        done = False  # In this continuous task, episodes don't really end
+        replay_buffer.add((
+            np.array(state, dtype=np.float32), 
+            np.array(new_state, dtype=np.float32), 
+            np.array([action], dtype=np.float32), 
+            np.array([last_reward], dtype=np.float32), 
+            np.array([done], dtype=np.float32)
+        ))
+        
+        # Train TD3 if we have enough samples
+        if len(replay_buffer.storage) > 100:  # Start training after 100 samples
+            brain.train(replay_buffer, iterations=1)  # Train for 1 iteration per step
+            
+        # Update scores and last distance
+        with torch.no_grad():
+            state_tensor = torch.FloatTensor(np.array(state).reshape(1, -1)).to(device)
+            action_tensor = torch.FloatTensor(np.array([action]).reshape(1, -1)).to(device)
+            scores.append(brain.critic.Q1(state_tensor, action_tensor).mean().item())
+        
+        last_distance = distance
+
+        # Update episode tracking
+        total_timesteps += 1
+        episode_reward += last_reward
+
+        # Print episode information at regular intervals
+        if total_timesteps % 1000 == 0:
+            print(f"Total Timesteps: {total_timesteps} Episode Num: {episode_num} Reward: {episode_reward}")
+            episode_reward = 0
+            episode_num += 1
+
+        # Evaluation step
+        if total_timesteps % EVALUATION_INTERVAL == 0:
+            avg_reward = sum(evaluation_rewards) / len(evaluation_rewards) if evaluation_rewards else 0
+            print("-" * 39)
+            print(f"Average Reward over the Evaluation Step: {avg_reward:.6f}")
+            print("-" * 39)
+            evaluation_rewards = []
+        else:
+            evaluation_rewards.append(last_reward)
+
+# Adding the painting tools
+
+class MyPaintWidget(Widget):
+
+    def on_touch_down(self, touch):
+        global length, n_points, last_x, last_y
+        with self.canvas:
+            Color(0.8,0.7,0)
+            d = 10.
+            touch.ud['line'] = Line(points = (touch.x, touch.y), width = 10)
+            last_x = int(touch.x)
+            last_y = int(touch.y)
+            n_points = 0
+            length = 0
+            sand[int(touch.x),int(touch.y)] = 1
+            img = PILImage.fromarray(sand.astype("uint8")*255)
+            img.save("./images/sand.jpg")
+
+    def on_touch_move(self, touch):
+        global length, n_points, last_x, last_y
+        if touch.button == 'left':
+            touch.ud['line'].points += [touch.x, touch.y]
+            x = int(touch.x)
+            y = int(touch.y)
+            length += np.sqrt(max((x - last_x)**2 + (y - last_y)**2, 2))
+            n_points += 1.
+            density = n_points/(length)
+            touch.ud['line'].width = int(20 * density + 1)
+            sand[int(touch.x) - 10 : int(touch.x) + 10, int(touch.y) - 10 : int(touch.y) + 10] = 1
+
+            
+            last_x = x
+            last_y = y
+
+# Adding the API Buttons (clear, save and load)
+
+class CarApp(App):
+
+    def build(self):
+        parent = Game()
+        parent.serve_car()
+        Clock.schedule_interval(parent.update, 1.0/60.0)
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text = 'clear')
+        savebtn = Button(text = 'save', pos = (parent.width, 0))
+        loadbtn = Button(text = 'load', pos = (2 * parent.width, 0))
+        clearbtn.bind(on_release = self.clear_canvas)
+        savebtn.bind(on_release = self.save)
+        loadbtn.bind(on_release = self.load)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        parent.add_widget(savebtn)
+        parent.add_widget(loadbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        global sand
+        self.painter.canvas.clear()
+        sand = np.zeros((longueur,largeur))
+
+    def save(self, obj):
+        print("saving brain...")
+        brain.save()
+        plt.plot(scores)
+        plt.show()
+
+    def load(self, obj):
+        print("loading last saved brain...")
+        brain.load()
+
+# Running the whole thing
+if __name__ == '__main__':
+    CarApp().run()
---
+++ td3.py
@@ -0,0 +1,175 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+# Selecting the device (CPU or GPU)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+        self.layer_1 = nn.Linear(state_dim, 400)
+        self.layer_2 = nn.Linear(400, 300)
+        self.layer_3 = nn.Linear(300, action_dim)
+        self.max_action = max_action
+
+    def forward(self, x):
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = self.max_action * torch.tanh(self.layer_3(x))
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        # Defining the first Critic neural network
+        self.layer_1 = nn.Linear(state_dim + action_dim, 400)
+        self.layer_2 = nn.Linear(400, 300)
+        self.layer_3 = nn.Linear(300, 1)
+        # Defining the second Critic neural network
+        self.layer_4 = nn.Linear(state_dim + action_dim, 400)
+        self.layer_5 = nn.Linear(400, 300)
+        self.layer_6 = nn.Linear(300, 1)
+
+    def forward(self, x, u):
+        xu = torch.cat([x, u], 1)
+        # Forward-Propagation on the first Critic Neural Network
+        x1 = F.relu(self.layer_1(xu))
+        x1 = F.relu(self.layer_2(x1))
+        x1 = self.layer_3(x1)
+        # Forward-Propagation on the second Critic Neural Network
+        x2 = F.relu(self.layer_4(xu))
+        x2 = F.relu(self.layer_5(x2))
+        x2 = self.layer_6(x2)
+        return x1, x2
+
+    def Q1(self, x, u):
+        xu = torch.cat([x, u], 1)
+        x1 = F.relu(self.layer_1(xu))
+        x1 = F.relu(self.layer_2(x1))
+        x1 = self.layer_3(x1)
+        return x1
+
+class ReplayBuffer(object):
+    def __init__(self, max_size=1e6):
+        self.storage = []
+        self.max_size = max_size
+        self.ptr = 0
+
+    def add(self, transition):
+        if len(self.storage) == self.max_size:
+            self.storage[int(self.ptr)] = transition
+            self.ptr = (self.ptr + 1) % self.max_size
+        else:
+            self.storage.append(transition)
+            self.ptr = (self.ptr + 1) % self.max_size
+
+    def sample(self, batch_size):
+        ind = np.random.randint(0, len(self.storage), size=batch_size)
+        batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = [], [], [], [], []
+        for i in ind:
+            state, next_state, action, reward, done = self.storage[i]
+            batch_states.append(np.array(state, copy=False))
+            batch_next_states.append(np.array(next_state, copy=False))
+            batch_actions.append(np.array(action, copy=False))
+            batch_rewards.append(np.array(reward, copy=False))
+            batch_dones.append(np.array(done, copy=False))
+        return np.array(batch_states), np.array(batch_next_states), np.array(batch_actions), np.array(batch_rewards).reshape(-1, 1), np.array(batch_dones).reshape(-1, 1)
+
+class TD3(object):
+    def __init__(self, state_dim, action_dim, max_action):
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = Critic(state_dim, action_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+        self.max_action = max_action
+        
+        # Exploration parameters
+        self.exploration_noise = 0.1
+        self.exploration_decay = 0.995
+        self.min_exploration = 0.01
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        action = self.actor(state).cpu().data.numpy().flatten()
+        
+        # Add exploration noise
+        noise = np.random.normal(0, self.exploration_noise, size=action.shape)
+        action = action + noise
+        action = np.clip(action, -self.max_action, self.max_action)
+        
+        # Decay exploration noise
+        self.exploration_noise = max(self.min_exploration, 
+                                   self.exploration_noise * self.exploration_decay)
+        
+        return action
+
+    def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, 
+              policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+        for it in range(iterations):
+            # Sample a batch of transitions from the replay buffer
+            batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+            
+            # Convert to tensors and ensure correct dimensions
+            state = torch.FloatTensor(batch_states).to(device)
+            next_state = torch.FloatTensor(batch_next_states).to(device)
+            action = torch.FloatTensor(batch_actions).squeeze(-1).to(device)
+            reward = torch.FloatTensor(batch_rewards).to(device)
+            done = torch.FloatTensor(batch_dones).to(device)
+
+            # Select next action according to target policy
+            next_action = self.actor_target(next_state)
+            noise = torch.randn_like(next_action) * policy_noise
+            noise = noise.clamp(-noise_clip, noise_clip)
+            next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
+
+            # Compute target Q-values
+            target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = reward + ((1 - done) * discount * target_Q).detach()
+
+            # Compute current Q-values
+            current_Q1, current_Q2 = self.critic(state, action)
+
+            # Compute critic loss with gradient clipping
+            critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.critic.parameters(), 1.0)
+            self.critic_optimizer.step()
+
+            # Delayed policy updates
+            if it % policy_freq == 0:
+                # Compute actor loss
+                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+
+                # Optimize the actor
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 1.0)
+                self.actor_optimizer.step()
+
+                # Update target networks
+                for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+                for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+    def save(self, filename, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
+        torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))
+
+    def load(self, filename, directory):
+        self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
+        self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename))) 
